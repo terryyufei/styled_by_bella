a9c91cd1845ae211122dcce8b9bbe971559966a3
Terry Wambui <shikuwambui@gmail.com>
2024-02-16 03:46:47 +0300

    Chore: (products)

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img class="m-5 pt-20" src="img/logo8.png" alt="logo">
+<img class="h-16 " src="img/logo8.png" alt="logo">
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,22 +1,24 @@
-<div class="container">
-    <h2>Products</h2>
-    <a href="{{ route('products.create') }}">Add New Product</a>
-    <div class="row">
-        @foreach ($products as $product)
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    @if($product->image)
-                        <img src="{{ asset('img/' . $product->image) }}" class="card-img-top" alt="{{ $product->name }}" style="object-fit: cover; height: 200px;">
-                    @endif
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $product->name }}</h5>
-                        <p class="card-text">${{ $product->price }}</p>
-                        <p class="card-text">Size: {{ $product->size }}</p>
-                        <a href="{{ route('products.show', $product->id) }}" class="btn btn-primary">View Details</a>
+<x-app-layout>
+    <div class="container p-5 mt-10">
+        
+        <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
+            @foreach ($products as $product)
+                <div class="max-w-sm mx-auto">
+                    <div class="bg-white shadow-lg hover:shadow-lg rounded-lg overflow-hidden">
+                        @if($product->image)
+                            <img src="{{ asset('img/' . $product->image) }}" class="w-full h-56 object-cover object-center" alt="{{ $product->name }}">
+                        @endif
+                        <div class="p-4">
+                            <h5 class="font-bold text-lg mb-2">{{ $product->name }}</h5>
+                            <p class="text-gray-700 text-base">${{ $product->price }}</p>
+                            <p class="text-gray-700 text-base">Size: {{ $product->size }}</p>
+                            <a href="{{ route('products.show', $product->id) }}" class="mt-4 block w-full bg-gradient-to-r from-yellow-400 to-yellow-900 hover:from-purple-500 hover:to-yellow-500 text-white font-bold py-2 px-4 rounded text-center">View Details</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
     
-</div>
+</x-app-layout>
+
