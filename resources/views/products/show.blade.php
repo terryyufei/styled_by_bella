7a7ced6ea04833e42e6de630f34f598f45f7143a
Terry Wambui <shikuwambui@gmail.com>
2024-02-19 04:51:55 +0300

    Chore: (Fix Product View)

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -9,10 +9,10 @@
                 </div>
             @endif
 
-            <div class="md:mt-60 text-gray-900">
-                <h1>{{ $product->name }}</h1>
-                <div>
-                    <strong>Price:</strong> ${{ $product->price }}
+            <div class="pt-5 md:mt-60 text-gray-700">
+                <h1 class="font-bold text-yellow-900 md:text-lg">{{ $product->name }}</h1>
+                <div class="text-purple-600 font-extrabold mt-2">
+                    <strong>$</strong> {{ $product->price }}
                 </div>
                 <div>
                     <strong>Size:</strong> {{ $product->size }}
@@ -21,20 +21,29 @@
                     <strong>Category:</strong> {{ $product->category->name ?? 'N/A' }}
                 </div>
 
-                <div class="md:flex justify-between pt-5">
+                <div class="md:flex gap-8 pt-5">
 
                     <a href="{{ route('products.index') }}">
-                        <x-primary-button class="ms-3 md:mt-5">Shop More</x-primary-button>
+                        <x-third-button class="ms-3 md:mt-5">Shop More
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                            </svg>
+
+                        </x-third-button>
                     </a>
 
                     <a href="">
                         <x-secondary-button class="ms-3 mt-5">
                             Add to Cart
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-                              </svg>
-                              
-                            
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                            </svg>
+
+
                         </x-secondary-button>
                     </a>
 
